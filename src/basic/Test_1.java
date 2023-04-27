33a6c91e84e1b37a4be69e8a3bb04a81f1e8d7d7
Dell <Dell@DESKTOP-2T7LK3V>
2023-04-27 20:07:37 +0530

    First Commit

---
+++ src/basic/Test_1.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class Test_1 {
+	
+public static void main(String[] args) {
+		
+		String a="123";
+		String rev="";
+		
+		for(int i=a.length()-1;i>=0;i--) {
+			
+			rev = rev + a.charAt(i);
+			}
+		
+		
+		if(a.equals(rev)) {
+			System.out.println(rev + ": This no is palindrom");
+		}
+		else {
+			System.out.println(rev + ": This no is not palindrom");
+		}
+		
+		
+	}
+
+
+}
